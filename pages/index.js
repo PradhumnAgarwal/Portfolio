056d54ec7ec7332276e0dfae9a182e34c3c2d68b
PradhumnAgarwal <pradhumnagarwal94@gmail.com>
2023-05-11 08:21:06 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { Footer, Navbar } from '../components';
+import { About, Explore, GetStarted, Hero } from '../sections';
+
+const Page = () => (
+  <div className="bg-primary-black overflow-hidden">
+    <Navbar />
+    <Hero />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="relative">
+      <GetStarted />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Page;
